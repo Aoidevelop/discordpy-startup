916a1f9795b21b13cf5358ab16c7d7f020eebed2
Aoidevelop <73771351+Aoidevelop@users.noreply.github.com>
2020-11-01 19:22:27 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,58 +1,56 @@
 import discord
-import os
-import traceback
-token = os.environ['DISCORD_BOT_TOKEN']  # トークンを入力してください。
+from janome.tokenizer import Tokenizer
+
+t = Tokenizer()
 
 client = discord.Client()
-
-ngwords = list()
-prefix = ('&')
+prefix = '&'
+ngword = []
 users = {}
+token = 'DISCORD_BOT_TOKEN'
+
+print('起動しました')
+
 
 @client.event
 async def on_message(message):
-    global users
     if message.author.bot:
         return
-
-    for n in ngwords:
-        if n in message.content.replace(' ', '').replace('　', ''):
-            await message.delete()
-            if str(message.author.id) not in users:
-                users[str(message.author.id)] = 1
-            else:
-                users[str(message.author.id)] += 1
-            await message.channel.send('その発言は許可されていません！')
-            break
+    if message.content[0] == prefix:
+        cmd = message.content.split()
+        if cmd[0] == prefix + 'help':
+            await message.channel.send('addword:NGワードを追加します'
+                                       'delword:NGワードを削除します'
+                                       'listword:NGワードの一覧を表示します'
+                                       'users:NGワードを発言したユーザーを表示します'
+                                       'help:このヘルプを表示します')
+        if cmd[0] == prefix + 'addword':
+            for arg in cmd[1::]:
+                if arg not in ngword:
+                    ngword.append(arg)
+            await message.channel.send(str(message.content.split()[1::]) + 'をNGワードに追加しました')
+        if cmd[0] == prefix + 'delword':
+            for arg in cmd[1::]:
+                if arg in ngword:
+                    ngword.remove(arg)
+            await message.channel.send(str(message.content.split()[1::]) + 'をNGワードから除外しました')
+        if cmd[0] == prefix + 'listword':
+            await message.channel.send(ngword)
+        if cmd[0] == prefix + 'users':
+            ans = ''
+            for u, c in users.items():
+                ans += client.get_user(u).name + 'さん '+ str(c) + '回'
+            await message.channel.send(ans)
     else:
-        if not message.author.permissions_in(message.channel).administrator:
-            return
-        if message.content.split()[0] == prefix + 'addword':
-            ngwords.append(message.content.split()[1])
-            await message.channel.send(message.content.split()[1] + 'をngワードに追加しました')
-        if message.content.split()[0] == prefix + 'delword':
-            ngwords.remove(message.content.split()[1])
-            await  message.channel.send(message.content.split()[1] + ('をngワードから削除しました'))
-        if message.content.split()[0] == prefix + 'wordlist':
-            await message.channel.send(ngwords)
-        if message.content.split()[0] == prefix + 'violator' and len(message.content.split()) == 2:
-            ans = {}
-            for u, p in users.items():
-                if int(message.content.split()[1]) < p:
-                    ans[u] = p
-            await  message.channel.send(ans)
-        elif message.content.split()[0] == prefix + 'violator':
-            await message.channel.send(users)
-        if message.content.split()[0] == prefix + 'purge':
-            users = {}
-        if message.content.split()[0] == prefix + 'help':
-            await  message.channel.send('ヘルプ\n'+
-                                        'addword <text>:ngワードを追加します。\n'+
-                                        'wordlist:ngワードの一覧を表示します\n'+
-                                        'violator <number>指定した回数以上ngワードを言った人を表示します。何も指定しないと0回以上になります。\n'+
-                                        'purge:ngワードを言った回数をリセットします\n'+
-                                        'help:helpを表示します。\n'+
-                                        'delword <text>:ngワードを削除します')
+        tokens = t.tokenize(message.content, wakati=True)
+        for token in tokens:
+            print(token)
+            if token in ngword:
+                await message.delete()
+                await message.channel.send('その発言は許可されていません')
+                if message.author.id not in users:
+                    users[message.author.id] = 1
+                else:
+                    users[message.author.id] += 1
 
 
-client.run(token)
